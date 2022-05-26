b2bd3c125d48fb3f9b07343b19f4aa3efc687bf1
Saurabh <saurabh.7teen@gmail.com>
2022-05-26 14:22:04 +0530

    permutation phone number - without global collection storage - Google interview question

--- Python/algos/problems/recursion/phone_number_permutations.py
+++ Python/algos/problems/recursion/phone_number_permutations.py
@@ -1,21 +1,31 @@
 # (Google) given a number combination eg "1,2" print all possible permutations
 # https://leetcode.com/problems/letter-combinations-of-a-phone-number/
 
+# Method 1 - using global number reference
 nums = {
-    "1": ["a", "b", "c"],
-    "2": ["d", "e", "f"]
+    "2": ["a", "b", "c"],
+    "3": ["d", "e", "f"],
+    "4": ["g", "h", "i"],
+    "5": ["j", "k", "l"],
+    "6": ["m", "n", "o"],
+    "7": ["p", "q", "r", "s"],
+    "8": ["t", "u", "v"],
+    "9": ["w", "x", "y", "z"],
 }
 
 
-def phone_number_permute(p, up):
+# Method 2 - without using global number reference
+def phone_number_permute_with_global(p, up):
     if not up:
-        print(p)
-        return
+        return [p]
 
-    processed_num = up[0]
-    letters = nums[processed_num]
-    for i in letters:
-        phone_number_permute(p + i, up[1:])
+    output = []
+    digit = int(up[0])
+    for i in range((3 * (digit - 1)), 3 * digit):
+        output.extend(phone_number_permute_with_global(p + chr(ord("a") + i), up[1:]))
+    return output
 
 
-phone_number_permute("", "12")
+# phone_number_permute("", "3")
+print(phone_number_permute_with_global("", "12"))
+# print(chr(ord("b") + 1))
