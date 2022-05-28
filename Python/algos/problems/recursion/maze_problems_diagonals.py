879685b4d0b203aeeab76001651309cc0518c865
Saurabh <saurabh.7teen@gmail.com>
2022-05-28 19:26:24 +0530

    put up obstacle in maze problem

--- Python/algos/problems/recursion/maze_problems_diagonals.py
+++ Python/algos/problems/recursion/maze_problems_diagonals.py
@@ -1,19 +1,21 @@
 # number of ways to reach the last block of matrix
 
 
-def count_ways(current_index, end_index):
+def count_ways(current_index, end_index, obstacle_index):
     if current_index == end_index:
         return 1
+    if current_index == obstacle_index:
+        return 0
     count = 0
 
     i, j = current_index
 
     if i < end_index[0]:
-        count += count_ways([i + 1, j], [2, 2])
+        count += count_ways([i + 1, j], [2, 2], obstacle_index)
     if j < end_index[1]:
-        count += count_ways([i, j + 1], [2, 2])
+        count += count_ways([i, j + 1], [2, 2], obstacle_index)
     if i < end_index[0] and j < end_index[1]:
-        count += count_ways([i + 1, j + 1], [2, 2])
+        count += count_ways([i + 1, j + 1], [2, 2], obstacle_index)
 
     return count
 
@@ -34,5 +36,5 @@ def print_ways(path, current_index, end_index):
     return path
 
 
-print(count_ways([0, 0], [2, 2]))
-print(print_ways("", [0, 0], [2, 2]))
+print(count_ways([0, 0], [2, 2], [1,1]))
+# print(print_ways("", [0, 0], [2, 2]))
