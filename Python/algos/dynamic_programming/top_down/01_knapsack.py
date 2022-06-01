55a74f13fad891a911420ce07309253b5ea2c18d
Saurabh <saurabh.7teen@gmail.com>
2022-06-01 19:43:03 +0530

    01 knapsack using recursive call with top down approach

--- Python/algos/dynamic_programming/top_down/01_knapsack.py
+++ Python/algos/dynamic_programming/top_down/01_knapsack.py
@@ -5,7 +5,18 @@ def find_max(weight_matrix, value_matrix, w):
     n = len(weight_matrix)
     matrix = [[None] * (w + 1) for i in range(n + 1)]
 
-    return matrix
+    for i in range(n + 1):
+        for j in range(w + 1):
+            if i == 0 or j == 0:
+                matrix[i][j] = 0
+            else:
+                if weight_matrix[i - 1] <= j:
+                    matrix[i][j] = max(weight_matrix[i - 1] + matrix[i - 1][j - weight_matrix[i - 1]],
+                                       matrix[i - 1][j])
+                else:
+                    matrix[i][j] = matrix[i - 1][j]
+
+    return matrix[n][w]
 
 
-print(find_max([1, 4, 5, 7], [1, 3, 4, 5], 7))
+print(find_max([1, 4, 5, 7], [1, 3, 4, 5], 14))
