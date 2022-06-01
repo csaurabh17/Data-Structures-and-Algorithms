891347af2c396bad0ce2ac191dc9728a7a5f000e
Saurabh <saurabh.7teen@gmail.com>
2022-06-01 14:23:33 +0530

    01 knapsack using recursive call with memoization done

--- Python/algos/dynamic_programming/01_knapsack.py
+++ Python/algos/dynamic_programming/01_knapsack.py
@@ -1,15 +1,26 @@
 # Solving 0-1 Knapsack
 # [1, 3, 4, 5] - [1, 4, 5, 7] - Knapsack weight is 7
 
+# with memoization - assuming n constraint is 100 and size is 1000
+t = [[-1] * 22 for i in range(22)]
+
+
 def find_max_profit(weight_arr, value_arr, size):
     if len(weight_arr) == 0 or size == 0:
         return 0
 
+    if t[size][len(weight_arr)] != -1:
+        return t[size][len(weight_arr)]
+
     if weight_arr[len(weight_arr) - 1] <= size:
-        return max(weight_arr[len(weight_arr) - 1] + find_max_profit(weight_arr[:-1], value_arr[:-1], size - weight_arr[len(weight_arr) - 1]),
-                   find_max_profit(weight_arr[:-1], value_arr[:-1], size))
+        t[size][len(weight_arr)] = max(
+            weight_arr[len(weight_arr) - 1] + find_max_profit(weight_arr[:-1], value_arr[:-1],
+                                                              size - weight_arr[len(weight_arr) - 1]),
+            find_max_profit(weight_arr[:-1], value_arr[:-1], size))
+        return t[size][len(weight_arr)]
     else:
         return find_max_profit(weight_arr[:-1], value_arr[:-1], size)
 
 
-print(find_max_profit([1, 4, 5, 7], [1, 3, 4, 5], 7))
+print(find_max_profit([1, 4, 5, 7], [1, 3, 4, 5], 14))
+
