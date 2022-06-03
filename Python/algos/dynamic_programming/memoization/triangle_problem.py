a485045b95d239adbe9c198a07b04cdf8aa41b00
Saurabh <saurabh.7teen@gmail.com>
2022-06-04 03:17:13 +0530

    re arranged problems

--- Python/algos/dynamic_programming/memoization/triangle_problem.py
+++ Python/algos/dynamic_programming/memoization/triangle_problem.py
@@ -6,7 +6,7 @@ def min_path(up, total, n, idx):
     i, j = idx
     if i == len(up) - 1:
         return total
-    if [i,j] == [0,0]:
+    if [i, j] == [0, 0]:
         total = up[i][j]
     min_value = min(min_path(up, total + up[i + 1][j], i + 1, [i + 1, j]),
                     min_path(up, total + up[i + 1][j + 1], i + 1, [i + 1, j + 1]))
@@ -14,4 +14,4 @@ def min_path(up, total, n, idx):
     return min_value
 
 
-print(min_path([[1], [2, 3], [3, 6, 7], [8, 9, 6, 10]], 0, 4, [0, 0]))
+print(min_path([[1], [2, 3], [3, 6, 7], [9, 8, 6, 1]], 0, 4, [0, 0]))
