e144c75b8b6cc24071a9e3683f985f99b352d43f
Saurabh <saurabh.7teen@gmail.com>
2022-06-04 15:05:34 +0530

    palindromic_subsequences.py

--- Python/algos/dynamic_programming/strings/palindromic_subsequences.py
+++ Python/algos/dynamic_programming/strings/palindromic_subsequences.py
@@ -9,14 +9,4 @@ def palindromic_subsequences(p, up):
     return max(palindromic_subsequences(p + up[0], up[1:]), palindromic_subsequences(p, up[1:]))
 
 
-def print_palindromic_subsequences(p, up):
-    if not up:
-        if p == p[::-1]:
-            return [p]
-        return []
-
-    return print_palindromic_subsequences(p + up[0], up[1:]) + print_palindromic_subsequences(p, up[1:])
-
-
 print(palindromic_subsequences("", "aabbbaa"))
-print(print_palindromic_subsequences("", "aabbbaa"))
